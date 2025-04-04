37059838618ca57e7cfd12928d0294b1907f3b80
Babaji Temkar <126382725+Satej-Temkar@users.noreply.github.com>
2025-04-04 15:57:19 +0530

    Update Update.jsx

--- mern-app-frontend/src/components/Update.jsx
+++ mern-app-frontend/src/components/Update.jsx
@@ -13,7 +13,7 @@ const Update = () => {
     const {id} = useParams();
   // get single user data 
   const getSingleUser = async () =>{
-    const response = await fetch(`http://localhost:5000/${id}`);
+    const response = await fetch(`https://mern-app-backend-e10e.onrender.com/${id}`);
     const result = await response.json();
 
     if(!response.ok){
